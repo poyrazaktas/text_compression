fa3d7b754ca8a4d86f79ee1ae149636da6e2d15f
POYRAZ AKTAS <poyraz.educational@gmail.com>
2020-05-15 01:59:23 +0300

    kod okunabilirligi arttirildi

--- main.c
+++ main.c
@@ -12,7 +12,8 @@
 #define GETLENGTH(x) (x & LENGTHMASK)
 #define OFFSETLENGTH(x,y) (x << LENGTHBITS | y)
 
-struct token {
+struct token
+{
     uint8_t offset_len;
     char c;
 };
@@ -22,7 +23,7 @@ struct token {
 * iki string'in ilk kaç karakteri özdeş?
 * maksimum limit sayısı kadar kontrol yapar.
 */
-static inline int prefix_match_length(char *s1, char *s2, int limit)
+static inline int prefix_eslesme_len(char *s1, char *s2, int limit)
 {
     int len = 0;
 
@@ -37,12 +38,11 @@ static inline int prefix_match_length(char *s1, char *s2, int limit)
 * kopyalama yapma garantisi olduğu için, bu
 * versiyonu kullanıyoruz.
 */
-static inline void lz77memcpy(char *s1, char *s2, int size)
-{
-    while (size--)
-        *s1++ = *s2++;
-}
-
+//static inline void lz77memcpy(char *s1, char *s2, int size)
+//{
+//    while (size--)
+//        *s1++ = *s2++;
+//}
 
 
 /*
@@ -55,22 +55,22 @@ static inline void lz77memcpy(char *s1, char *s2, int size)
 * Eğer numTokens NULL değilse, token sayısını
 * numTokens ile gösterilen yere kaydeder.
 */
-static struct token *encode(char *text, int limit, int *numTokens)
+static struct token *encode(char *text, int limit, int *token_sayisi)
 {
     // cap (kapasite) hafızada kaç tokenlik yer ayırdığımız.
-    int cap = 1 << 3;
+    int kapasite = 1 << 3;
 
     // kaç token oluşturduğumuz.
-    int _numTokens = 0;
+    int _tokenSayisi = 0;
 
     // oluşturulacak token
-    struct token t;
+    struct token ornek;
 
     // lookahead ve search buffer'larının başlangıç pozisyonları
     char *tampon_ileri, *tampon_arama;
 
     // tokenler için yer ayır.
-    struct token *encoded = malloc(cap * sizeof(struct token));
+    struct token *encoded = malloc(kapasite * sizeof(struct token));
 
     // token oluşturma döngüsü
     for (tampon_ileri = text; tampon_ileri < text + limit; tampon_ileri++)
@@ -86,21 +86,21 @@ static struct token *encode(char *text, int limit, int *numTokens)
 
         // search bufferda bulunan en uzun eşleşmenin
         // boyutu
-        int max_len = 0;
+        int boyut_eslesme = 0;
 
         // search bufferda bulunan en uzun eşleşmenin
         // pozisyonu
-        char *max_match = tampon_ileri;
+        char *metin_eslesme = tampon_ileri;
 
         // search buffer içinde arama yap.
         for (; tampon_arama < tampon_ileri; tampon_arama++)
         {
-            int len = prefix_match_length(tampon_arama, tampon_ileri, LENGTHMASK);
+            int len = prefix_eslesme_len(tampon_arama, tampon_ileri, LENGTHMASK);
 
-            if (len > max_len)
+            if (len > boyut_eslesme)
             {
-                max_len = len;
-                max_match = tampon_arama;
+                boyut_eslesme = len;
+                metin_eslesme = tampon_arama;
             }
         }
 
@@ -110,33 +110,34 @@ static struct token *encode(char *text, int limit, int *numTokens)
         * tokenin içine bir karakter koyabilmek için, eşleşmeyi kısaltmamız
         * gerekiyor.
         */
-        if (tampon_ileri + max_len >= text + limit)
+        if (tampon_ileri + boyut_eslesme >= text + limit)
         {
-            max_len = text + limit - tampon_ileri - 1;
+            boyut_eslesme = text + limit - tampon_ileri - 1;
         }
 
         printf("ileri tampon: %s\n",tampon_ileri);
         // bulunan eşleşmeye göre token oluştur.
-        t.offset_len = OFFSETLENGTH(tampon_ileri - max_match-1, max_len);
-        printf("###: %d %d %c\n",tampon_ileri-max_match,max_len,tampon_ileri[max_len]);
-        tampon_ileri += max_len;
+        ornek.offset_len = OFFSETLENGTH(tampon_ileri - metin_eslesme-1, boyut_eslesme);
 
-        t.c = *tampon_ileri;
+        printf("###: %d %d %c\n",tampon_ileri-metin_eslesme,boyut_eslesme,tampon_ileri[boyut_eslesme]);
+
+        tampon_ileri += boyut_eslesme;
+        ornek.c = *tampon_ileri;
 
         // gerekirse, hafızada yer aç
-        if (_numTokens + 1 > cap)
+        if (_tokenSayisi + 1 > kapasite)
         {
-            cap = cap << 1;
-            encoded = realloc(encoded, cap * sizeof(struct token));
+            kapasite = kapasite << 1;
+            encoded = realloc(encoded, kapasite * sizeof(struct token));
         }
 
         // oluşturulan tokeni, array'e kaydet.
 
-        encoded[_numTokens++] = t;
+        encoded[_tokenSayisi++] = ornek;
     }
 
-    if (numTokens)
-        *numTokens = _numTokens;
+    if (token_sayisi)
+        *token_sayisi = _tokenSayisi;
 
     return encoded;
 }
@@ -154,48 +155,48 @@ static char *file_read(FILE *f, int *size)
     fread(content, 1, *size, f);
     return content;
 }
-int karakter_sayisi(FILE *f )
+int num_characters(FILE *f )
 {
     f=fopen("source.txt","r");
-    int count=0;
+    int sayac=0;
     char c;
-    for(c=getc(f);c!=EOF;c=getc(f))
-        count++;
+    for(c=getc(f); c!=EOF; c=getc(f))
+        sayac++;
     fclose(f);
-    return count;
+    return sayac;
 }
 int main(void)
 {
-    FILE *f;
-    int metin_boyutu = 56, token_sayisi, decode_boyutu;
-    char *kaynak_metin = "Bu kose yaz kosesi bu kose kis kosesi ortadaki su sisesi", *decoded_metin = "";
-    struct token *encoded_metin;
+    FILE *file;
+    int text_size = 56, num_tokens, decode_size;
+    char *text = "Bu kose yaz kosesi bu kose kis kosesi ortadaki su sisesi", *decoded_text = "";
+    struct token *encoded_text;
 
-    if (f = fopen("source.txt", "rb"))
+    if (file = fopen("source.txt", "rb"))
     {
-        kaynak_metin = file_read(f, &metin_boyutu);
-        fclose(f);
+        text = file_read(file, &text_size);
+        fclose(file);
     }
 
 
-    kaynak_metin[karakter_sayisi(f)]='\0';
-    encoded_metin = encode(kaynak_metin, metin_boyutu, &token_sayisi);
+    text[num_characters(file)]='\0';
+    encoded_text = encode(text, text_size, &num_tokens);
 
-    if (f = fopen("encoded.txt", "wb"))
+    if (file = fopen("encoded.txt", "wb"))
     {
-        fwrite(encoded_metin, sizeof(struct token), token_sayisi, f);
-        fclose(f);
+        fwrite(encoded_text, sizeof(struct token), num_tokens, file);
+        fclose(file);
     }
 
-/*
-    decoded_metin = decode(encoded_metin, token_sayisi, &decode_boyutu);
+    /*
+        decoded_metin = decode(encoded_metin, token_sayisi, &decode_boyutu);
 
-    if (f = fopen("decoded.txt", "wb"))
-    {
-        fwrite(decoded_metin, 1, decode_boyutu, f);
-        fclose(f);
-    }
-*/
-    printf("\nOrjinal Boyut: %d, Encode Boyutu: %d", metin_boyutu, token_sayisi * sizeof(struct token));
+        if (f = fopen("decoded.txt", "wb"))
+        {
+            fwrite(decoded_metin, 1, decode_boyutu, f);
+            fclose(f);
+        }
+    */
+    printf("\nOrjinal Boyut: %d, Encode Boyutu: %d", text_size, num_tokens * sizeof(struct token));
     return 0;
 }
